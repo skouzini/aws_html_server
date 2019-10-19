6028ec5df458a44cb578357f1f964436ee085312
Josh Skousen <skousenjosh@gmail.com>
2019-10-18 18:07:33 -0600

    First commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+var app = new Vue({
 
+  el: '#app',
+
+  data: {
+    number: '',
+    current: {
+      title: '',
+      img: '',
+      alt: ''
+    },
+    loadind: true;
+  },
+
+  created() {
+    this.xkcd();
+  },
+
+  methods: {
+    async xkcd() {
+      try {
+        this.loading = true;
+        const response = await axios.get('https://xkcdapi.now.sh/' +
+          thisre.number);
+        this.current = response.data;
+        this.loading = false;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+  }
+});
